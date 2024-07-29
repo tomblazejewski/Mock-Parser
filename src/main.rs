c12fa530e70d2bb59d7b292093bd79d577db6f39
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-07-29 21:44:14 +0100

    Change main routine

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,15 @@ use chumsky::prelude::*;
 fn main() {
     let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
 
-    println!("{:?}", parser().parse(src))
+    match parser().parse(src) {
+        Ok(ast) => match eval(&ast) {
+            Ok(output) => println!("{}", output),
+            Err(eval_err) => println!("Evaluation error: {}", eval_err),
+        },
+        Err(parse_errs) => parse_errs
+            .into_iter()
+            .for_each(|e| println!("Parse error: {}", e)),
+    }
 }
 
 #[derive(Debug)]
